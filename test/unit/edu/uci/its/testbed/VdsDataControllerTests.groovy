cf60a1398c89e644ba15a1caf86453c344481a72
Craig Rindt <crindt@uci.edu>
2010-05-03 12:54:51 -0700

    Added some tests that still need to be fleshed out.

---
+++ test/unit/edu/uci/its/testbed/VdsDataControllerTests.groovy
@@ -0,0 +1,17 @@
+package edu.uci.its.testbed
+
+import grails.test.*
+
+class VdsDataControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
