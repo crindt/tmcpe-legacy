244157d04ba18efdf74b7af5465accd11096085d
Craig Rindt <crindt@uci.edu>
2010-07-15 16:14:32 -0700

    Added additional help text in the How To Use the Site section.  Also, improved the summary statistics table on the home page to pull in delay statistics.  Refs #211.

--- grails-app/controllers/edu/uci/its/tmcpe/HomeController.groovy
+++ grails-app/controllers/edu/uci/its/tmcpe/HomeController.groovy
@@ -28,17 +28,24 @@ class HomeController {
         types.each() { typ ->
             log.info( typ + " ::::: " + typ.class )
             def tt = Incident.executeQuery( "select distinct count(i.cad),min( i.startTime ), max( i.startTime ) from Incident i where i.eventType = '" + typ + "'" )
-            def ta = Incident.executeQuery("select distinct count(i.cad),min( i.startTime ), max( i.startTime ) from Incident i where i.eventType = '" + typ + "' AND i.analyses.size > 0" )
+//            def ta = Incident.executeQuery("select distinct count(i.cad),min( i.startTime ), max( i.startTime ) from Incident i where i.eventType = '" + typ + "' AND i.analyses.size > 0" )
+            def ta = IncidentFacilityImpactAnalysis.executeQuery("select count(ifia.incidentImpactAnalysis.incident),min( ifia.incidentImpactAnalysis.incident.startTime ), max( ifia.incidentImpactAnalysis.incident.startTime ),sum(ifia.netDelay) from IncidentFacilityImpactAnalysis ifia where ifia.incidentImpactAnalysis.incident.eventType = '" + typ + "'" )
             stats[typ] = [
                 "tot": tt[0],
-                "analyzed": ta[0]
+                "analyzed": ta[0][0],
+                "delay": ta[0][3],
+                "savings": 0
             ]
         }
         log.info( stats )
 
+        def ta = IncidentFacilityImpactAnalysis.executeQuery("select count(ifia.incidentImpactAnalysis.incident),min( ifia.incidentImpactAnalysis.incident.startTime ), max( ifia.incidentImpactAnalysis.incident.startTime ),sum(ifia.netDelay) from IncidentFacilityImpactAnalysis ifia" )
+        def ta2 = IncidentFacilityImpactAnalysis.executeQuery("select count(ifia.incidentImpactAnalysis.incident),sum(ifia.netDelay) from IncidentFacilityImpactAnalysis ifia" )
         stats["tot"] = [
             "tot": Incident.executeQuery("select distinct count(i.cad),min( i.startTime ), max( i.startTime ) from Incident i" )[0],
-            "analyzed": Incident.executeQuery("select distinct  count(i.cad),min( i.startTime ), max( i.startTime ) from Incident i where i.analyses.size > 0" )[0]
+            "analyzed": ta[0],
+            "delay": ta2[0][1],
+            "savings": 0
             ]
 /*
         stats.values().each() { 
