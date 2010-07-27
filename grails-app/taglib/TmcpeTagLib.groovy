71a0acef94e85a419855d08473b11a947d9c7eb7
Craig Rindt <crindt@uci.edu>
2010-07-27 05:01:22 -0700

    Release 0.2.4

--- grails-app/taglib/TmcpeTagLib.groovy
+++ grails-app/taglib/TmcpeTagLib.groovy
@@ -65,5 +65,5 @@ class TmcpeTagLib {
       out << '<script>dojo.registerModulePath("tmcpe","' << resource( dir:'js/tmcpe',file:'' ) << '");</script>\n'
   }
 
-  def version = { out << '0.2.5' }
+  def version = { out << '0.2.4' }
 }
