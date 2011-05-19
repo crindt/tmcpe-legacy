98773612061ddbd9155f22c98166292829c16757
Craig Rindt <crindt@translab.its.uci.edu>
2011-05-19 16:22:52 -0700

    Tweaked link in incident detail on map page to open analysis in new window.  this was lost in recent commits

--- web-app/js/tmcpe/map.js
+++ web-app/js/tmcpe/map.js
@@ -329,7 +329,7 @@ if ( !tmcpe ) var tmcpe = {};
 	  for ( var i = new_page_index; i<max; ++i ){
 	      var id = $('#incidentDetail');
 	      id.append('<h3>Incident '+cluster.elements[i].data.properties.cad+'</h3>');
-	      id.append('<p><a href="incident/d3_tsd?cad='+cluster.elements[i].data.properties.cad+'">Show detailed analysis</a></p>');
+	      id.append('<p><a target="_blank" href="incident/d3_tsd?cad='+cluster.elements[i].data.properties.cad+'">Show detailed analysis</a></p>');
 	  }
 	  
 	  // select the first element
