e1f167e629b25ca8e34b4714016bc712c8bee527
Craig Rindt <crindt@translab.its.uci.edu>
2011-08-08 17:26:18 -0700

    Fixed some table styling problems on resize and scroll.

--- web-app/js/tmcpe/tmcpe-revamp.js
+++ web-app/js/tmcpe/tmcpe-revamp.js
@@ -1,39 +1,3 @@
-// These are from here: http://www.softcomplex.com/docs/get_window_size_and_scrollbar_position.html
-function f_clientWidth() {
-	return f_filterResults (
-		window.innerWidth ? window.innerWidth : 0,
-		document.documentElement ? document.documentElement.clientWidth : 0,
-		document.body ? document.body.clientWidth : 0
-	);
-}
-function f_clientHeight() {
-	return f_filterResults (
-		window.innerHeight ? window.innerHeight : 0,
-		document.documentElement ? document.documentElement.clientHeight : 0,
-		document.body ? document.body.clientHeight : 0
-	);
-}
-function f_scrollLeft() {
-	return f_filterResults (
-		window.pageXOffset ? window.pageXOffset : 0,
-		document.documentElement ? document.documentElement.scrollLeft : 0,
-		document.body ? document.body.scrollLeft : 0
-	);
-}
-function f_scrollTop() {
-	return f_filterResults (
-		window.pageYOffset ? window.pageYOffset : 0,
-		document.documentElement ? document.documentElement.scrollTop : 0,
-		document.body ? document.body.scrollTop : 0
-	);
-}
-function f_filterResults(n_win, n_docel, n_body) {
-	var n_result = n_win ? n_win : 0;
-	if (n_docel && (!n_result || (n_result > n_docel)))
-		n_result = n_docel;
-	return n_body && (!n_result || (n_result > n_body)) ? n_body : n_result;
-}
-
 // Creates a bounded scale...FIXME: crindt: clarify the implementation here...
 function boundedScale( dom, ran ) {
     var dommin = dom[0];
@@ -77,6 +41,7 @@ if ( !tmcpe ) var tmcpe = {};
 
   po = org.polymaps;
 
+  ////////////////////////////////////////////////////////////////////////////////
   // Top level query object.  This loads the data from the remote server.  It's
   // basically the model for this page
   tmcpe.query = function() {
@@ -104,6 +69,7 @@ if ( !tmcpe ) var tmcpe = {};
 	  
       }
 
+      // Setting the url executes the query
       query.url = function(x) {
 	  if ( !arguments.length ) return url;
 	  url = x;
@@ -119,15 +85,17 @@ if ( !tmcpe ) var tmcpe = {};
       return query;
   }
 
+  ////////////////////////////////////////////////////////////////////////////////
   // We'll store the master list of data in the table
-  var df = d3.time.format("%Y-%m-%d %H:%M");
   tmcpe.tableView = function() {
       var tableView = {}
       ,element
       ,thead
       ,tbody
+      ,tfoot
       ,container
-      ,renderDate = function( o ) { return df(new Date( o )); }
+      ,dateFormat = d3.time.format("%Y-%m-%d %H:%M")
+      ,renderDate = function( o ) { return dateFormat(new Date( o )); }
       ,renderNumber = function( o ) { return ( o != null ? o.toFixed(0) : o ); }
       ,fields = [ {key:"cad",title:"CAD"}, 
 		  {key:"timestamp",title:"Time",render:renderDate}, 
@@ -141,9 +109,10 @@ if ( !tmcpe ) var tmcpe = {};
 
       // constructors and private methods
       function init() {
-	  if ( container == null ) return;
+	  // If the container doesn't fit, you must aquit
+	  if ( container == null ) return; 
 
-	  // empty container
+	  // Clear container
 	  var divs = container.selectAll('div');
 	  divs.remove();
 
@@ -157,11 +126,19 @@ if ( !tmcpe ) var tmcpe = {};
 	      .html(function(d){return d.title;});
 
 	  tbody = element.append('tbody');
-	  resetColumnWidths();
 	  $('#incident-list')
 	      .dataTable({bPaginate:false,"bAutoWidth":false})
 	      .fnFilter(true,null,true,false) // Use regex filtering, not "smart"
 	  ;
+
+	  tfoot = element.append('tfoot');
+	  hrow = tfoot.append('tr');
+	  hrow.selectAll('th').data(fields).enter()
+	      .append('th')
+	      .attr('class',function(d){return d.key==null?"":d.key;})
+	      .html(function(d){return d.key=="memo" ? "Totals:" : ""; });
+	  
+	  resetColumnWidths();
       }
 
 
@@ -177,8 +154,7 @@ if ( !tmcpe ) var tmcpe = {};
 		  return d.cad
 	      } )
 	      .attr("class", function(d,i) { return i % 2 ? "even" : "odd"; } )
-	      .attr("style", function(d,i) { 
-		  return "background-color:"+color(160)(d.properties.tmcpe_delay)/*+";opacity:0.5"*/; })
+	      .style("background-color",function(d){return color(160)(d.properties.tmcpe_delay)})
 	      .on("click",function(d,e) { 
 		  $(window).trigger( "tmcpe.incidentSelected", d );
 	      } )
@@ -218,24 +194,25 @@ if ( !tmcpe ) var tmcpe = {};
       tableView.container = function(x) {
 	  if ( !arguments.length ) return container;
 	  container = x;
-	  init();
+	  // If we change the container, we need to reinit everything?
+	  // Could we just detach nodes and move them to the new?
+	  init();  
 	  return tableView;
       }
 
       tableView.data = function(x) {
 	  if ( !arguments.length ) { 
 	      // map out the data from the dom
+	      // fixme: confirm this works
 	      return d3.select(container).filter('tbody tr').data();
 	  }
-	  update(x);
+	  update(x); // new data means we must update
 	  return tableView;
       }
 
 
       // event handlers
       tableView.incidentSelected = function( incident ) {
-	  // scroll if not visible
-
 	  highlightIncidentRow( incident );
       }
 
@@ -243,44 +220,81 @@ if ( !tmcpe ) var tmcpe = {};
       function highlightIncidentRow(incident) {
 	  //d3.select(e.currentTarget.parent).each( function(d) { d.addClass
 	  $('tr.selected').removeClass('selected')
-	  $("tr[cad~='"+incident.cad+"']").addClass('selected');
+	  var newRow = $("tr[cad~='"+incident.cad+"']");
+	  newRow.addClass('selected');
+
+	  // get the top of the row and scroll it if it isn't visible
+	  var rowOffset = newRow.offset();
+	  var scrollTop = $(window).scrollTop()
+	  var winheight = $(window).height();
+	  var rowheight = newRow.height();
+	  var loc = rowOffset.top - scrollTop;
+	  var calc = (winheight-(21)-rowheight);
+	  if ( loc < 0 || loc > calc ) {
+	      var newTop = rowOffset.top-(21+40-3);
+	      $(window).scrollTop( newTop );
+	  }
+	  
+
 	  //	    $(e.currentTarget.parentElement).addClass('selected');
       }
 
+      tableView.resetColumnWidths = function() {
+	  resetColumnWidths();
+      }
+
+
       // utility methods
       function resetColumnWidths() {
+	  var wid = $(window).width();
+	  var table_fudge=100;
+
+	  $('#incident-list').width(wid-250-table_fudge);
+
 	  // On this, we pretty much want to resize the memo column and that's it...
-	  var wid = f_clientWidth();
 	  var cwid = $(container[0]).width();
 	  var mwid = $('#incident-list .memo').width();
+	  var mwid_min = $('#incident-list td .memo').css('min-width');
 	  var widsum = 0;
 	  var hold=[];
 	  _.each(['cad','timestamp','locString','memo','d12_delay','tmcpe_delay','savings'],
 		 function( d ) {
-		     var twid = $('#incident-list td.'+d).width();
-		     widsum += $('#incident-list td.'+d).width()
+		     var twid = $('#incident-list tbody td.'+d).width();
+		     widsum += twid;
 		     hold[d] = twid;
 		 });
-	  var table_fudge=100;
-	  var avwid = wid-250-table_fudge-(widsum-mwid);
+//	  var avwid = cwid-(widsum);
+	  var avwid = wid-table_fudge-250-(widsum-mwid);
 
-	  $('#incident-list .memo').css('max-width',avwid+'px');
-	  $('#incident-list .memo').css('width',avwid+'px');
+	  if ( avwid < mwid_min || avwid < 200 ) avwid = 200;
+	  var sel = $('#incident-list .memo').css('width',avwid+'px').css('max-width',avwid+'px');
 	  _.each(['cad','timestamp','locString','d12_delay','tmcpe_delay','savings'],
 		 function( d ) {
-		     $('#incident-list thead th.'+d).css('width',hold[d]+'px');
-		     $('#incident-list thead th.'+d).css('max-width',hold[d]+'px');
+		     $('#incident-list thead th.'+d)
+			 .css('width',hold[d]+'px')
+			 .css('min-width',hold[d]+'px')
+			 .css('max-width',hold[d]+'px')
+		     ;
+		     $('#incident-list tfoot th.'+d)
+			 .css('width',hold[d]+'px')
+			 .css('min-width',hold[d]+'px')
+			 .css('max-width',hold[d]+'px')
+		     ;
 		 });
       }
 
       return tableView;
   }
 
+
+
+  ////////////////////////////////////////////////////////////////////////////////
   // Instantiates and manages the map display of the incidents in the query
   tmcpe.query.mapView = function()    {  
       var mapView = {}
       ,container
       ,map   // the polymaps map 
+      ,selectedCluster
       ;
 
       // create the base map
@@ -341,6 +355,11 @@ if ( !tmcpe ) var tmcpe = {};
 		      });
 		      point.setAttribute('fill', color(0)(_.max(arr)));
 
+		      
+                      var more_wider = value.data.properties.elements.length * 2;
+                      point.setAttribute("r", polymaps_cluster.base_radius() + more_wider );
+
+
 		      // set the title, for tooltips?
 		      point.setAttribute("title",props.elements.length+" incident" + (props.elements.length==1 ? "" : "s") );
 
@@ -352,7 +371,7 @@ if ( !tmcpe ) var tmcpe = {};
 		      // Add a click handler
 		      $(point).click(function(e) {
 			  // Call local function to handle this...
-			  clusterClicked( value.data );
+			  clusterClicked( value.data.properties );
 		      });
 		  }
 	      } ))
@@ -393,20 +412,34 @@ if ( !tmcpe ) var tmcpe = {};
 
       // external event handlers
       mapView.incidentSelected = function( d ) {
-	  //alert( "Incident Selected " + d.cad );
+	  // If an incident has been selected, we want to select the associated cluster...
 
 	  // Get the cluster for this cad
 	  sel = $('circle[cads~="'+d.cad+'"]');	  
 
 	  if ( sel.length == 0 ) alert( "Cluster not found for " + d.cad );
 
-	  // propogate
+	  /*
+	  // find the index of the selected incident in the cluster
+	  var elIndex;
+	  for ( elIndex = 0; sel[0].data.elements[elIndex].cad != d.cad && elIndex < sel[0].data.elements[elIndex].length; ++elIndex );
+
+	  if ( elIndex == sel[0].data.elements.length ) { 
+	      alert( "Element " + d.cad + " Not found in cluster!" ); 
+	      return;
+	  } 
+
+	  // propogate as a global tmcpe event
+	  var cldata = {cluster:sel[0].data,elementIndex:elIndex};
+	  */
 	  $(window).trigger( 'tmcpe.clusterSelected', sel[0].data );
       }
 
       mapView.clusterSelected = function(d) {
+	  // don't propagate if we've already selected this cluster
+	  if ( d == selectedCluster ) return;
 
-	  clusterSelected(sel[0].data);
+	  clusterSelected(d);
       }
 
       function raiseIncident( elem ) {
@@ -472,6 +505,9 @@ if ( !tmcpe ) var tmcpe = {};
 	  
 	  // highlight cluster
 	  highlightIncident( cad_elem );
+
+	  // record the current selection...
+	  selectedCluster = e;
 	  
       };
 
@@ -481,11 +517,14 @@ if ( !tmcpe ) var tmcpe = {};
   }
 
   
+  ////////////////////////////////////////////////////////////////////////////////
   // Instantiates and manages the incident detail as a function of cluster
   tmcpe.query.detailView = function() {
       var detailView = {}
       ,container
       ,list
+      ,selectedCluster
+      ,selectedIncident
       ;
 
       function init() {
@@ -509,6 +548,7 @@ if ( !tmcpe ) var tmcpe = {};
 	  li.enter()
 	      .append("li")
 	      .attr('cad',function(d){ return d.data.cad; })
+	      .on('click',function(d){ $(window).trigger('tmcpe.incidentSelected',d.data); })
 	      .html(function(d){ return d.data.cad; });
 
 	  li.exit().remove();
@@ -532,11 +572,18 @@ if ( !tmcpe ) var tmcpe = {};
 
       // event handlers
       detailView.clusterSelected = function(d) {
+	  if ( d == selectedCluster ) return;
+
+	  selectedCluster = d;
+
 	  detailView.data( d.elements );
+	  
+	  //$(window).trigger( 'tmcpe.incidentSelected', d.elements[0].data );
       }
 
-      detailView.incidentSelected = function(d) {
-	  container.selectAll("li[cad~='"+d.cad+"']").style("color","yellow");
+      detailView.incidentSelected = function( d ) {
+	  $(container[0]).find("li.selected").removeClass('selected');
+	  $(container[0]).find("li[cad~='"+d.cad+"']").addClass('selected');//style("color","yellow");
       }
 
       return detailView;
@@ -574,11 +621,16 @@ if ( !tmcpe ) var tmcpe = {};
       } );
 
 
+
+      $(window).resize(function(e) {
+	  tableView.resetColumnWidths();
+      });
+
+
       /* gmail-like hack to fix specific elements once we've scrolled past a point */
       $(window).scroll(function(e) { 
 
-	  var scrollTop =f_scrollTop();
-	  if ( scrollTop > (21+60) ) {
+	  if ( $(window).scrollTop() > (21+60) ) {
 	      // We've scrolled such that the banner should disappear
 	      // fix the map and thead a the top of the page
 	      $('#leftbox').css({'position':'fixed','top':21,'left':'0px'});
@@ -587,24 +639,39 @@ if ( !tmcpe ) var tmcpe = {};
 						     +5 // content margin
 						     +10)  // content padding
 					     +'px'});
+	      $('#incident-list tfoot').css({'position':'fixed','bottom':'0px',
+					     'left':(250 // width of map
+						     +5 // content margin
+						     +10)  // content padding
+					     +'px'});
+
+	      
+	      
 
 	      // now we need to adjust the th widths because they tend to get out of sync
+	      /*
 	      _.each(['cad','timestamp','locString','memo','d12_delay','tmcpe_delay','savings'],
 		     function( d ) {
-			 wid = $('#incident-list td.'+d).css('width');
+			 wid = $('#incident-list tr:first-child td.'+d).css('width');
 			 hwid = $('#incident-list th.'+d).css('width');
-			 maxwid = $('#incident-list td.'+d).css('max-width');
-			 hmaxwid = $('#incident-list th.'+d).css('max-width');
-			 $('#incident-list thead th.'+d).css('width',wid);
-			 $('#incident-list thead th.'+d).css('max-width',maxwid);
+			 maxwid = $('#incident-list tr:first-child td.'+d).css('min-width');
+			 hmaxwid = $('#incident-list th.'+d).css('min-width');
+			 $('#incident-list thead th.'+d).css('width',wid).css('min-width',maxwid);
+			 $('#incident-list tfoot th.'+d).css('width',wid).css('min-width',maxwid);
 		     });
+		     */
+	      tableView.resetColumnWidths();
 	      
-	  } else if ( scrollTop <= (21+60) ) {
+	  }
+	  if ( $(window).scrollTop() <= (21+60) ) {
 	      // OK, the banner should be visible now, ditch the fixed positions
 
 	      $('#leftbox').css({'position':'absolute','top':'101px','left':'0px'});
 	      $('#content').css({'position':'absolute','left':'250px'});
 	      $('#incident-list thead').css({'position':'static'});
+	  }
+	  if ( $(window).scrollTop() >= ( $(document).height() - $(window).height() - $('#incident-stats').height() ) ) {
+	      $('#incident-list tfoot').css({'position':'static'});
 	  }
       })
 
@@ -615,7 +682,5 @@ if ( !tmcpe ) var tmcpe = {};
 	  .query()
 	  .url('/tmcpe/incident/list.geojson?startDate=2010-09-01&endDate=2011-01-01&Analyzed=onlyAnalyzed&max=1000');
   });
-
-
 
  })();
