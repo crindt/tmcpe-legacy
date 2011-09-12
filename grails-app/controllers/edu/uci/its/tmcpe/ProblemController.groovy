e97b3b2bed91afc6de399db523cfd220b9b7a001
Craig Rindt <crindt@translab.its.uci.edu>
2011-09-12 13:16:55 -0700

    Minor formatting changes.  And for whatever reason, FFox user agent string encoding on report problem link appears to be working.  So fixes #865.

--- grails-app/controllers/edu/uci/its/tmcpe/ProblemController.groovy
+++ grails-app/controllers/edu/uci/its/tmcpe/ProblemController.groovy
@@ -4,17 +4,10 @@ import java.net.URLEncoder
 import org.codehaus.groovy.grails.plugins.codecs.URLCodec
 
 class ProblemController {
-	
-	// Get security service reference via dependency injection
-	def springSecurityService
-
-    static navigation = [
-        [group: 'dashboard', order:97, title:'Report Problem', action: 'report', isVisible: { springSecurityService.isLoggedIn() }],
-        ]
 
 	def report = {
 	    def url = URLEncoder.encode( "issue[description]=User Agent: "+request.getHeader("User-Agent") +'\n\n', URLCodec.getEncoding()) 
-		redirect(target:"_blank", url:'http://localhost/redmine/projects/tmcpe/issues/new?'+url)
+	    redirect(target:"_blank", url:'http://localhost/redmine/projects/tmcpe/issues/new?'+url)
 	}
 	
 	
